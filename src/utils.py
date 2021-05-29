20f6f317423b284ce8ff05f310efc7341101cee9
arsyad <38887752+Arsyad-ah@users.noreply.github.com>
2021-05-29 18:02:21 +0800

    added util functions

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+from ast import literal_eval
 
 def create_folder(path):
     if not os.path.exists(path):
@@ -6,3 +7,12 @@ def create_folder(path):
     return True
 
 
+def _cfg_to_dict(cfg, section_name):
+    results = {}
+    for opt in cfg.options(section_name):
+        try:
+            results[opt] = literal_eval(cfg[section_name].get(opt))
+        except:
+            results[opt] = cfg[section_name].get(opt)
+
+    return results
