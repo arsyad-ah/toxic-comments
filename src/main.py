d10e84a08debba19ad4398c3fd95252373adb136
arsyad <38887752+Arsyad-ah@users.noreply.github.com>
2021-05-30 11:46:38 +0800

    added print statements and argparser

--- src/main.py
+++ src/main.py
@@ -13,12 +13,16 @@ RUN_TIME = NOW.strftime('%H%M%S')
 def main(config):
     
     objective = config.get('DEFAULT', 'objective')
+    print('objective: ' , objective)
 
     if objective == 'prepare':
        
         pipeline = DataPipeline(config, RUN_TIME)
+        print('Reading data')
         pipeline.read_data('raw_data')
+        print('Cleaning data')
         pipeline.clean_data()
+        print('Preparing data')
         pipeline.prepare()
 
     elif objective == 'train':
@@ -34,24 +38,20 @@ if __name__ == '__main__':
     import argparse
     from configparser import ConfigParser, ExtendedInterpolation
 
-    # parser = argparse.ArgumentParser(
-    #     prog='toxic',
-    #     description='Params to run main function',
-    # )
-    # print('adding args')
-    # parser.add_argument(
-    #     "--config_path",
-    #     help="Full path to config file",
-    #     required=True,
-    #     default='./config/config.ini',
-    # )
+    parser = argparse.ArgumentParser(
+        prog='toxic',
+        description='Params to run main function')
 
-    path = './config/config.ini'
-    print('init args')
-    # args = parser.parse_args()
+    parser.add_argument(
+        "--config_path",
+        help="Full path to config file",
+        default='./config/config.ini',
+    )
+
+    args = parser.parse_args()
     config = ConfigParser(interpolation=ExtendedInterpolation())
     print('config load ok')
-    config.read(path)
-    # config.read(args.config_path)
+    config.read(args.config_path)
 
+    print('executing run')
     main(config)
