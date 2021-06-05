1737c41eca5bee55a11ab3e4a85370ede7e4aa94
arsyad <38887752+Arsyad-ah@users.noreply.github.com>
2021-06-05 13:57:01 +0800

    changed configparser

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+import configparser
 from ast import literal_eval
 
 def create_folder(path):
@@ -7,12 +8,25 @@ def create_folder(path):
     return True
 
 
-def _cfg_to_dict(cfg, section_name):
+def _cfg_to_dict(cfg, section_name, no_defaults=False, **kwargs):
     results = {}
-    for opt in cfg.options(section_name):
+    for opt in cfg.options(section_name, no_defaults=no_defaults, **kwargs):
+        print(opt, section_name)
         try:
             results[opt] = literal_eval(cfg[section_name].get(opt))
         except:
             results[opt] = cfg[section_name].get(opt)
 
     return results
+
+class ConfigParser(configparser.ConfigParser):
+    """Can get options() without defaults
+    """
+    def options(self, section, no_defaults=False, **kwargs):
+        if no_defaults:
+            try:
+                return list(self._sections[section].keys())
+            except KeyError:
+                raise configparser.NoSectionError(section)
+        else:
+            return super().options(section, **kwargs)
