26416a43fec90cc764912d9913307b4e18e7d185
arsyad <38887752+Arsyad-ah@users.noreply.github.com>
2021-06-05 14:01:30 +0800

    renamed cfg_to_dict

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import logging
 from src.datapipeline import DataPipeline
 from src.train import train
-# from src.inference import make_inference
+from src.inference import make_inference
 from datetime import datetime
-from src.utils import _cfg_to_dict, ConfigParser
+from src.utils import ConfigParser
 
 
 NOW = datetime.now()
