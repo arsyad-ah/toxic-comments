370e4ac7555643162b192bab1fd54b1e95138750
arsyad-ah <arsyad.a.h@gmail.com>
2023-04-01 23:20:56 +0800

    to download embeddings

---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+filename=glove.6B.zip
+embeddings_dir=../embeddings
+
+curl https://nlp.stanford.edu/data/${filename} -O $embeddings_dir/$filename
+unzip $embeddings_dir/$filename -d .
